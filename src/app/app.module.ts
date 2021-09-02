c89eb07a2e3aba261dcd358f3aba92de3e696d76
davi-souza2001 <davisouza2001dv@gmail.com>
2021-09-02 11:24:40 -0300

    Earth description

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { DescriptionComponent } from './components/description/description.component';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    DescriptionComponent,
+    BodyComponent,
   ],
   imports: [
     BrowserModule,
