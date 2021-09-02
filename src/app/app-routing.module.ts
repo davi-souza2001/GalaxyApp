6654b0b7f653db1190cac18ca6f516e4d9592cfe
davi-souza2001 <davisouza2001dv@gmail.com>
2021-09-02 14:01:22 -0300

    commit planets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DescriptionComponent } from './components/description/description.component';
+import { MercuryComponent } from './components/mercury/mercury.component';
+import { VenusComponent } from './components/venus/venus.component';
+import { MarsComponent } from './components/mars/mars.component';
+import { JupiterComponent } from './components/jupiter/jupiter.component';
+import { SaturnComponent } from './components/saturn/saturn.component';
+import { UranusComponent } from './components/uranus/uranus.component';
+import { NeptuneComponent } from './components/neptune/neptune.component';
+
+
+const routes: Routes = [
+  {
+    path: "",
+    component: DescriptionComponent
+  },{
+    path: "mercury",
+    component: MercuryComponent
+  },{
+    path: "venus",
+    component: VenusComponent
+  },{
+    path: "mars",
+    component: MarsComponent
+  },{
+    path: "jupiter",
+    component: JupiterComponent
+  },{
+    path: "saturn",
+    component: SaturnComponent
+  },{
+    path: "uranus",
+    component: UranusComponent
+  },{
+    path: "neptune",
+    component: NeptuneComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DescriptionComponent } from './components/description/description.component';
-import { BodyComponent } from './components/body/body.component';
+import { MercuryComponent } from './components/mercury/mercury.component';
+import { MainComponent } from './components/main/main.component';
+import { VenusComponent } from './components/venus/venus.component';
+import { MarsComponent } from './components/mars/mars.component';
+import { JupiterComponent } from './components/jupiter/jupiter.component';
+import { SaturnComponent } from './components/saturn/saturn.component';
+import { UranusComponent } from './components/uranus/uranus.component';
+import { NeptuneComponent } from './components/neptune/neptune.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     DescriptionComponent,
-    BodyComponent,
+    MercuryComponent,
+    MainComponent,
+    VenusComponent,
+    MarsComponent,
+    JupiterComponent,
+    SaturnComponent,
+    UranusComponent,
+    NeptuneComponent,
   ],
   imports: [
     BrowserModule,
